7f3457d865d410b9f58d6a5bef5f460512a55b8e
GEORGE <ellouly@hotmail.com>
2020-06-25 12:09:14 +0200

    Entities Album, Artist, Category and database created

---
+++ migrations/Version20200625093606.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200625093606 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE album (id INT AUTO_INCREMENT NOT NULL, artist_id INT NOT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, year INT DEFAULT NULL, image VARCHAR(255) NOT NULL, titles VARCHAR(255) NOT NULL, link_audio VARCHAR(255) NOT NULL, INDEX IDX_39986E43B7970CF8 (artist_id), INDEX IDX_39986E4312469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE artist (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE album ADD CONSTRAINT FK_39986E43B7970CF8 FOREIGN KEY (artist_id) REFERENCES artist (id)');
+        $this->addSql('ALTER TABLE album ADD CONSTRAINT FK_39986E4312469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE album DROP FOREIGN KEY FK_39986E43B7970CF8');
+        $this->addSql('ALTER TABLE album DROP FOREIGN KEY FK_39986E4312469DE2');
+        $this->addSql('DROP TABLE album');
+        $this->addSql('DROP TABLE artist');
+        $this->addSql('DROP TABLE category');
+    }
+}
